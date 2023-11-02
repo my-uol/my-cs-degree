01542f6e7eee408ba963b4f7dc20c605cd9fa7cc
Matheus Colombo <matheus.colombo@janeladigital.com>
2023-11-02 15:30:02 +0000

    New index layout + Structuring

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using my_cs_degree.Models;
+using Newtonsoft.Json;
+using my_cs_degree.Models;
+using System.Net;
 
 namespace my_cs_degree.Controllers;
 
@@ -13,9 +16,38 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        string gitHubApi = "https://api.github.com/repos/my-uol/my-cs-degree/commits";
+
+        try
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("User-Agent", "C# App");
+                client.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
+                client.DefaultRequestHeaders.Add("Authorization", "token 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");
+
+                var response = await client.GetAsync(gitHubApi);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    // start from the json parameter commit
+                    var json = await response.Content.ReadAsStringAsync();
+                    var commits = JsonConvert.DeserializeObject<List<GitHubCommitModel>>(json);
+
+                    return View(commits);
+                }
+                else
+                {
+                    return View();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            return View();
+        }
     }
 
     public IActionResult Privacy()
---
+++ Models/GitHubCommitModel.cs
@@ -0,0 +1,35 @@
+using Newtonsoft.Json;
+using System;
+
+namespace my_cs_degree.Models
+{
+    public class GitHubCommitModel
+    {
+        [JsonProperty("commit")]
+        public CommitDetails Commit { get; set; }
+
+        [JsonProperty("html_url")]
+        public string Url { get; set; }
+    }
+
+    public class CommitDetails
+    {
+        [JsonProperty("author")]
+        public AuthorModel Author { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+    }
+
+    public class AuthorModel
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("date")]
+        public DateTime Date { get; set; }
+    }
+}
