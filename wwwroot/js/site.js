52d49958b4f37355c5ecbfc497fa8e05d336e6f1
per-sa <ccolombomatheus@gmail.com>
2023-11-12 22:04:31 +0000

    Refactor GradeCalculator functions to minimize copy-paste #1

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,48 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function calculateGrades(grades, weights) {
+    grades = grades.map(grade => grade ?? 0);
+    weights = weights.map(weight => weight ?? 1);
 
-// Write your JavaScript code.
+    const [gradeOne, gradeTwo, gradeThree, gradeFour, gradeFive, gradeSix, gradeSeven, gradeEight] = grades;
+    const [weightOne, weightTwo, weightThree, weightFour, weightFive, weightSix, weightSeven, weightEight] = weights;
+
+    let averageMidterm = ((gradeOne * weightOne) + (gradeTwo * weightTwo) + (gradeThree * weightThree) + (gradeFour * weightFour) + (gradeFive * weightFive) + (gradeSix * weightSix) + (gradeSeven * weightSeven) + (gradeEight * weightEight));
+    let averageFinal = gradeSeven;
+    let overallGrade = ((averageMidterm + averageFinal) / 2);
+
+    document.getElementById("averageMidterm").innerHTML = averageMidterm.toFixed(2);
+    document.getElementById("averageFinal").innerHTML = averageFinal.toFixed(2);
+    document.getElementById("overallGrade").innerHTML = overallGrade.toFixed(2);
+
+    function setGradeClass(elementId, grade) {
+        let className = 'm-auto ';
+        if (grade >= 40) {
+            className += 'table-success';
+        } else if (grade >= 35) {
+            className += 'table-warning';
+        } else {
+            className += 'table-danger';
+        }
+        document.getElementById(elementId).className = className;
+    }
+
+    setGradeClass("overallGrade", overallGrade);
+    setGradeClass("averageFinal", averageFinal);
+    setGradeClass("averageMidterm", averageMidterm);
+}
+
+function triggerGradeCalculation() {
+    let gradeIds = ["gradeOne", "gradeTwo", "gradeThree", "gradeFour", "gradeFive", "gradeSix", "gradeSeven", "gradeEight"];
+    let weightIds = ["weightOne", "weightTwo", "weightThree", "weightFour", "weightFive", "weightSix", "weightSeven", "weightEight"];
+
+    let grades = gradeIds.map(id => {
+        let element = document.getElementById(id);
+        return element ? parseInt(element.value) : 0;
+    });
+
+    let weights = weightIds.map(id => {
+        let element = document.getElementById(id);
+        return element ? parseFloat(element.innerHTML) : 0;
+    });
+
+    calculateGrades(grades, weights);
+}
