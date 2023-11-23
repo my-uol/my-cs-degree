19fa1095dd816aa935456b982dfee26fb13fce07
Matheus Colombo <matheus.colombo@janeladigital.com>
2023-11-23 12:58:13 +0000

    Identity views and classes

---
+++ Areas/Identity/Data/my_cs_degreeContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using my_cs_degree.Areas.Identity.Data;
+
+namespace my_cs_degree.Data;
+
+public class my_cs_degreeContext : IdentityDbContext<my_cs_degreeUser>
+{
+    public my_cs_degreeContext(DbContextOptions<my_cs_degreeContext> options)
+        : base(options)
+    {
+    }
+    
+    public DbSet<my_cs_degreeUser> my_cs_degreeUser { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
---
+++ Areas/Identity/Data/my_cs_degreeUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace my_cs_degree.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the my_cs_degreeUser class
+public class my_cs_degreeUser : IdentityUser
+{
+    [PersonalData]  
+    [Column(TypeName ="nvarchar(100)")]  
+    public string Firstname { get; set; }  
+    [PersonalData]  
+    [Column(TypeName = "nvarchar(100)")]  
+    public string LastName { get; set; }  
+}
+
