2aff5b95ec6f42e5f0510abc25612cafd7935e21
Matheus Colombo <matheus.colombo@janeladigital.com>
2023-11-23 12:57:43 +0000

    Misc, identity and DbContext

--- Program.cs
+++ Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using my_cs_degree.Areas.Identity.Data;
+using my_cs_degree.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+builder.Services.AddRazorPages();
 
+
+builder.Services.AddDbContext<my_cs_degreeContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("default")));
+
+builder.Services.AddDefaultIdentity<my_cs_degreeUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<my_cs_degreeContext>();
 
 var app = builder.Build();
 
@@ -13,17 +25,18 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-DotNetEnv.Env.Load();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
